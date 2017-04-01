48432a8a28d79ac786f33b1094ef81296ee23d9c
unknown <shenchen3@staff.sina.com.cn>
2017-04-01 15:05:18 +0800

    Add exercise in wekk 5.

---
+++ week5_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int digits = 0;
+	/*
+	printf("sizeof(char) = %d\n", sizeof(char));
+	printf("sizeof(short) = %d\n", sizeof(short));
+	printf("sizeof(int) = %d\n", sizeof(int));
+	printf("sizeof(double) = %d\n", sizeof(double));
+	printf("sizeof(long long) = %d\n", sizeof(long long));
+	*/
+	short a = 32897;
+	char b = 32897;
+	// b = a;
+	printf("%d", b);
+	return 0;
+}
+
